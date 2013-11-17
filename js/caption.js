cd73888cdcd59cf3dcd76c4f1b178fdc19340253
Julie Wiederhold <j_wiederhold@comcast.net>
2013-11-16 21:20:12 -0800

    First Draft - works

---
+++ js/caption.js
@@ -0,0 +1,8 @@
+var Caption = {
+	captionText: [
+		{
+			// stop-----------UPPER------>	  -------*UPPER---------> --------------------*>
+			text: ['How long in lower case low low low', 'How long in lower case low low lo']
+		}
+	]
+}
---
+++ js/insert-caption.js
@@ -0,0 +1,24 @@
+function render(captionText) {
+	var $template = $('.template');
+	var $caption = $('.text-captioning');
+
+	$caption.empty();
+
+	var idx;
+
+	for(idx = 0; idx < captionText.length; ++idx){
+		var $clone = $template.clone();
+		var capText = captionText[idx];
+		$clone.find('.top-line').html(capText.text[0]);
+		$clone.find('.bottom-line').html(capText.text[1]);
+	}
+	
+
+	$clone.removeClass('template');
+	$caption.append($clone);
+}
+
+$(function(){
+	var captionText = Caption.captionText;
+	render(captionText);
+});
