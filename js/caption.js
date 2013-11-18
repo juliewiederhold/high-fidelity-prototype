5f54539e8d08e9ba2569083346d4996794af36aa
Julie Wiederhold <j_wiederhold@comcast.net>
2013-11-18 14:38:10 -0800

    11/18 - added on radar pictures

--- js/caption.js
+++ js/caption.js
@@ -2,8 +2,9 @@ var Caption = {
 	captionText: [
 		{
 			topText: 'Mute Mode',
-			// stop-----------UPPER------>	  -------*UPPER---------> --------------------*>
-			text: ['Hello, this is a long string of text to get an idea of what we want', 'to appear on the glasses. We can really write a lot which is awesome!']
+			// stop------------------------------------------------------------------->    
+			text: ['Hello, this is a long string of text to get an idea of what we want', 
+				   'to appear on the glasses. We can really write a lot which is awesome!']
 		}
 	]
 }
