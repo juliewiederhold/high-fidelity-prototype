db339c7f1c5800ea60ff868b77eb30e9f9156e16
Julie Wiederhold <j_wiederhold@comcast.net>
2013-11-19 16:30:38 -0800

    11/18

--- js/caption.js
+++ js/caption.js
@@ -1,7 +1,7 @@
 var Caption = {
 	captionText: [
 		{
-			topText: 'Mute Mode',
+			topText: '',
 			// stop------------------------------------------------------------------->    
 			text: ['Hello, this is a long string of text to get an idea of what we want', 
 				   'to appear on the glasses. We can really write a lot which is awesome!']
